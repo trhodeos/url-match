7e72a7c5f15a4cd9235a95ca7dace7550070a642
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2014-07-29 22:06:02 -0700

    Simplify, simplify, simplify.

--- url-match.js
+++ url-match.js
@@ -17,41 +17,35 @@
     factory(window['um'] = {});
   }
 } (function(exp, require) {
+  
   /**
-   * @param patternString {!String} The url match pattern that this instance represents.
-   * @constructor
+   * @param pattern {!String}
    */
-  var Pattern = function(patternString) {
+  var Matcher = function(patternString) {
     this.patternString_ = patternString;
+
+    this.isValid.bind(this);
+    this.getMatcherRegex_.bind(this);
+    this.matches.bind(this);
   };
 
   // split this regex out for regex-clarity.
-  Pattern.VALID_ALL_URL_REGEX = /^<all_urls>$/;
+  Matcher.VALID_ALL_URL_REGEX = /^<all_urls>$/;
   // taken from http://code.tutsplus.com/tutorials/8-regular-expressions-you-should-know--net-6149
-  Pattern.ALL_URL_REGEX = /^(\*|http|https|file|ftp|chrome-extension):\/\/([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
-  Pattern.VALID_MATCHER_REGEX = /^(\*|http|https|file|ftp|chrome-extension):\/\/(\*|(\*\.)?[^\/\*]+)?\/(.*)$/;
+  Matcher.ALL_URL_REGEX = /^(\*|http|https|file|ftp|chrome-extension):\/\/([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+  Matcher.VALID_MATCHER_REGEX = /^(\*|http|https|file|ftp|chrome-extension):\/\/(\*|(\*\.)?[^\/\*]+)?\/(.*)$/;
 
-  /**
-   * Is this pattern valid with regards to 
-   * https://developer.chrome.com/extensions/match_patterns
-   * @return true if valid, false otherwise
-   */
-  Pattern.prototype.isValid = function() {
-    return Pattern.VALID_ALL_URL_REGEX.test(this.patternString_) 
-        || Pattern.VALID_MATCHER_REGEX.test(this.patternString_);   
+  Matcher.prototype.isValid = function() {
+    return Matcher.VALID_ALL_URL_REGEX.test(this.patternString_) 
+        || Matcher.VALID_MATCHER_REGEX.test(this.patternString_);   
   };
 
-  /**
-   * Turns this pattern into a valid javascript regexp.
-   * @return {!RegExp}
-   */
-  Pattern.prototype.toRegExp = function() {
-    //TODO(tyler.s.rhodes): validity check.
-    if (!this.regExp_ && this.isValid()) {
-      if (Pattern.VALID_ALL_URL_REGEX.test(this.patternString_)) {
-        this.regExp_ = Pattern.ALL_URL_REGEX;
+  Matcher.prototype.getMatcherRegex_ = function(patternString) {
+    if (!this.matcher_ && this.isValid()) {
+      if (Matcher.VALID_ALL_URL_REGEX.test(this.patternString_)) {
+        this.regExp_ = Matcher.ALL_URL_REGEX;
       } else {
-        var matchResults = Pattern.VALID_MATCHER_REGEX.exec(this.patternString_);
+        var matchResults = Matcher.VALID_MATCHER_REGEX.exec(this.patternString_);
         var output = '/^';
         output += matchResults[1].replace(/\*/, '[^:]*');
         output += ':\\/\\/';
@@ -59,41 +53,20 @@
           output += matchResults[2].replace(/\./, '\\.').replace(/\*/, '[^\\/]*');
         }
         output += "\\/.*$";
-        this.regExp_ = new RegExp(output);
+        this.matcher_ = new RegExp(output);
       }
     }
-
-    return this.regExp_;
+    return this.matcher_;
   };
 
-  var PatternFactory = {};
-  /**
-   * @param obj {!Pattern|String} Object to create a pattern for.
-   */
-  PatternFactory.create = function(obj) {
-    if (obj instanceof Pattern) {
-      return obj;
-    }
-
-    // TODO(tyler.s.rhodes): caching of some sort
-    return new Pattern(obj);
-  };
-
-  /**
-   * @param pattern {!Pattern|String}
-   */
-  var Matcher = function(pattern) {
-    this.pattern_ = PatternFactory.create(pattern);
-    if (!this.pattern_.isValid()) {
-      // TODO(tyler.s.rhodes): what to do?
-    }
-  };
   Matcher.prototype.matches = function(string) {
-    return true;
+    if (!this.isValid()) {
+      console.log("Pattern " + this.patternString_ + " is not valid! Not matching any strings.");
+      return false;
+    }
+    return this.getMatcherRegex_().test(string);
   };
 
   // exports!
   exp.Matcher = Matcher;
-  exp.Pattern = Pattern;
-  exp.PatternFactory = PatternFactory;
 }));
--- url-match_test.js
+++ url-match_test.js
@@ -55,55 +55,43 @@ var validCases = {
 }
 
 module.exports = {
-  Pattern: {
-    testValidity: function(test) {
-      for (var curCase in validCases.keys) {
-        test.ok(new um.Pattern(curCase).isValid(),
-            curCase + " should be valid, but is not.");
-      }
-      test.done();
-    },
-      
-    testInvalidity: function(test) {
-      var cases = [
-        "http://www.google.com",
-        "http://*foo/bar",
-        "http://foo.*.bar/baz",
-        "http:/bar",
-        "foo://*"
-      ];
-      for (var i = 0; i < cases.length; i++) {
-        test.ok(!new um.Pattern(cases[i]).isValid(),
-            cases[i] + " shouldn't be valid, but is.");
-      }
-      test.done();
+  testValidity: function(test) {
+    for (var curCase in validCases.keys) {
+      test.ok(new um.Matcher(curCase).isValid(),
+          curCase + " should be valid, but is not.");
     }
+    test.done();
   },
-  PatternFactory: {
 
-  },
-  Matcher: {
-    testBasicMatching: function(test) {
-      var matcher = new um.Matcher("http://*.google.com");
-      test.ok(matcher.matches("http://www.google.com"));
-      test.done();
-    },
-    testMatching: function(test) {
-      for (var key in validCases) {
-        console.log("Testing " + key);
-        var curCase = validCases[key];
-        var matcher = new um.Matcher(key);
-        for (var i = 0; i < curCase.valid; i++) {
-          var testString = curCase.valid[i];
-          test.ok(matcher.matches(testString), key + " should match " + testString + ", but does not!");
-        }
-        for (var i = 0; i < curCase.invalid; i++) {
-          var testString = curCase.invalid[i];
-          test.ok(!matcher.matches(testString), key + " shouldn't match " + testString + ", but does!");
-        }
-      }
-      test.done();
-    
+  testInvalidity: function(test) {
+    var cases = [
+      "http://www.google.com",
+    "http://*foo/bar",
+    "http://foo.*.bar/baz",
+    "http:/bar",
+    "foo://*"
+      ];
+    for (var i = 0; i < cases.length; i++) {
+      test.ok(!new um.Matcher(cases[i]).isValid(),
+          cases[i] + " shouldn't be valid, but is.");
     }
+    test.done();
   },
+
+  testMatching: function(test) {
+    for (var key in validCases) {
+      console.log("Testing " + key);
+      var curCase = validCases[key];
+      var matcher = new um.Matcher(key);
+      for (var i = 0; i < curCase.valid; i++) {
+        var testString = curCase.valid[i];
+        test.ok(matcher.matches(testString), key + " should match " + testString + ", but does not!");
+      }
+      for (var i = 0; i < curCase.invalid; i++) {
+        var testString = curCase.invalid[i];
+        test.ok(!matcher.matches(testString), key + " shouldn't match " + testString + ", but does!");
+      }
+    }
+    test.done();
+  }
 };
